6daefd97ff09990d4bee28b8d5d3389d01cca446
PecoraroAmal <amal.pecoraro@gmail.com>
2025-11-04 09:45:50 +0100

    1.2

--- sw.js
+++ sw.js
@@ -1,21 +1,21 @@
-const CACHE_NAME = 'Test-cache-v1.1';
+const CACHE_NAME = 'Test-cache-v1.2';
 const urlsToCache = [
-  '/Test/index.html?v=1.1',
-  '/Test/edit.html?v=1.1',
-  '/Test/privacy-policy.html?v=1.1',
-  '/Test/cookie-policy.html?v=1.1',
-  '/Test/info.html?v=1.1',
-  '/Test/informazioni.html?v=1.1',
-  '/Test/download.html?v=1.1',
-  '/Test/scarica.html?v=1.1',
-  '/Test/css/style.css?v=1.1',
-  '/Test/css/policy.css?v=1.1',
-  '/Test/js/crypto.js?v=1.1',
-  '/Test/js/edit.js?v=1.1',
-  '/Test/js/home.js?v=1.1',
-  '/Test/js/info.js?v=1.1',
-  '/Test/js/app.js?v=1.1',
-  '/Test/js/utils.js?v=1.1',
+  '/Test/index.html?v=1.2',
+  '/Test/edit.html?v=1.2',
+  '/Test/privacy-policy.html?v=1.2',
+  '/Test/cookie-policy.html?v=1.2',
+  '/Test/info.html?v=1.2',
+  '/Test/informazioni.html?v=1.2',
+  '/Test/download.html?v=1.2',
+  '/Test/scarica.html?v=1.2',
+  '/Test/css/style.css?v=1.2',
+  '/Test/css/policy.css?v=1.2',
+  '/Test/js/crypto.js?v=1.2',
+  '/Test/js/edit.js?v=1.2',
+  '/Test/js/home.js?v=1.2',
+  '/Test/js/info.js?v=1.2',
+  '/Test/js/app.js?v=1.2',
+  '/Test/js/utils.js?v=1.2',
   '/Test/assets/apple-touch-icon.png',
   '/Test/assets/Test-logo.png',
   '/Test/assets/favicon-96x96.png',
@@ -81,11 +81,11 @@ self.addEventListener('fetch', event => {
           return fetchAndUpdateCache(event.request);
         }
         // If offline and not in cache, return fallback
-        return caches.match('/Test/index.html?v=1.1');
+        return caches.match('/Test/index.html?v=1.2');
       })
       .catch(error => {
         console.error('Fetch failed:', error);
-        return caches.match('/Test/index.html?v=1.1');
+        return caches.match('/Test/index.html?v=1.2');
       })
   );
 });
