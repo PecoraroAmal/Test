989a2c338248719bbddae22ac59b751b5598a80c
PecoraroAmal <amal.pecoraro@gmail.com>
2025-11-07 17:27:02 +0100

    3.5

--- js/crypto.js
+++ js/crypto.js
@@ -9,10 +9,10 @@ let argon2Ready = false;
 
 const argon2ReadyPromise = new Promise((resolve, reject) => {
     const script = document.createElement('script');
-    script.src = '/Test/lib/argon2.min.js';
+    script.src = '/Test/tree/main/lib/argon2.min.js';
     script.onload = () => {
         loadArgon2Module({
-            wasmUrl: '/Test/lib/argon2.wasm',
+            wasmUrl: '/Test/tree/main/lib/argon2.wasm',
             onAbort: reject
         }).then(mod => {
             argon2Module = mod;
